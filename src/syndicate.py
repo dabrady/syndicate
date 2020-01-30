968ed21f620dfae46e05f717cdc9fbb728058998
Daniel Brady <daniel.brady@tapjoy.com>
2020-01-30 12:04:11 +0000

    Using multiline string as workaround for input seq
    - update workflow info
    
    - fix python log
    
    - dummy commit: i don't think it worked
    
    peek at the available environment

--- src/syndicate.py
+++ src/syndicate.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 from datetime import datetime
 
-print(f"You want to publish to these places: {sys.argv[1:-1]}")
+print(f"You want to publish to these places? {sys.argv[1:]}")
+
+print("We have access to these environment variables:")
+print(os.environ)
+
 print(f"::set-output name=time::{datetime.now()}")
