eca7b54c6f9e119cea8c916777dd09b9d9ded9aa
Daniel Brady <daniel.brady@tapjoy.com>
2020-01-31 14:03:26 +0000

    Add dummy silo recognition

--- syndicate/__init__.py
+++ syndicate/__init__.py
@@ -1,10 +1,17 @@
 import sys
 import os
 from datetime import datetime
+import importlib.util
 
 def elsewhere(silos):
-    print(f"You want to publish to these places? {silos}")
+    print(f"You want to publish to these places: {silos}")
+
+    print("Do I know how?")
+    recognized_silos = {silo:bool(importlib.util.find_spec(f"syndicate.silos.{silo}")) for silo in silos}
+    print(recognized_silos)
+
     print(f"Do we have the necessary API keys?")
     available_keys = {silo:(f"{silo}_API_KEY" in os.environ) for silo in silos }
     print(available_keys)
+
     print(f"::set-output name=time::{datetime.now()}")
---
+++ syndicate/silos/dev.py
@@ -0,0 +1,1 @@
+print("Hello? Yes, this is DEV.")
