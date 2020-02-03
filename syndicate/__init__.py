c2edcec1ec62c08ee78b4c735a59e19d52a7e163
Daniel Brady <daniel.brady@tapjoy.com>
2020-02-03 18:46:25 +0000

    Compress syndication

--- syndicate/__init__.py
+++ syndicate/__init__.py
@@ -1,8 +1,9 @@
 from syndicate.utils import action_log, action_warn
 
-import sys
-import os
+import functools
 import importlib.util
+import os
+import sys
 
 def elsewhere(posts, silos):
     if not silos:
@@ -11,42 +12,38 @@ def elsewhere(posts, silos):
 
     action_log(f"You want to publish to these places: {silos}")
 
-    specs = {silo:_locate(silo) for silo in silos}
-    recognized_silos = {silo:spec for (silo,spec) in specs.items() if spec}
-    available_keys = {silo:_has_api_key(silo) for silo in recognized_silos.keys()}
+    specs = {silo:_locate(silo) for silo in silos if _locate(silo)}
+    if specs.keys() != silos:
+        action_warn(f"I don't know how to publish to these places: { [silo for silo in silos if silo not in specs] }")
 
-    if recognized_silos and any(available_keys.values()):
-        action_log(f"I know how to publish to these places: {list(recognized_silos.keys())}")
-        action_log("I'll do what I can.")
-        if not all(available_keys.values()):
-            action_log(f"But I don't have API keys for these places: {[silo for (silo, available) in available_keys.items() if not available]}")
+    api_keys = {silo:_get_api_key(silo) for silo in silos if _get_api_key(silo)}
+    if api_keys.keys() != silos:
+        action_warn(f"I don't have API keys for these places: { [silo for silo in silos if silo not in api_keys] }")
 
-        # TODO just operate on silos for which we have API keys
-        results = {
-            silo:_syndicate(spec, _get_api_key(silo), posts)
-            for (silo,spec) in specs.items()
-            if _has_api_key(silo)
-        }
-
+    action_log("I'll do what I can.")
+    results = {
+        silo:_syndicate(spec, api_keys[silo], posts)
+        for (silo, spec) in specs.items()
+        if silo in api_keys
+    }
+    if results:
         return results
     else:
-        action_warn("Sorry, can't help you.")
+        action_warn("Sorry, can't do anything with that.")
         return None
 
 ### privates ###
 _API_KEY = lambda s: f"{s.upper()}_API_KEY"
 
+@functools.lru_cache(max_size=10)
 def _locate(silo):
-    return importlib.util.find_spec(f'syndicate.silos.{silo.lower()}')
+    return importlib.util.find_spec(f'syndicate.silos.{silo.lower()}').getattr('name', None)
 
 def _syndicate(silo_spec, api_key, posts):
     if silo_spec and api_key:
         return importlib.import_module(silo_spec.name).syndicate(posts, api_key)
     else:
         return None
-
-def _has_api_key(silo):
-    return _API_KEY(silo) in os.environ
 
 def _get_api_key(silo):
     return os.getenv(_API_KEY(silo))
