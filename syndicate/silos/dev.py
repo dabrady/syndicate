d70561f26f46a8b9247be0f20c90ffc905b537b3
Daniel Brady <daniel.brady@tapjoy.com>
2020-02-03 20:08:25 +0000

    Delete unused DEV feature: 'fetch'

--- syndicate/silos/dev.py
+++ syndicate/silos/dev.py
@@ -18,35 +18,6 @@ def syndicate(posts, api_key):
 
 ## This is a simple semantic wrapper around the DEV API, currently in beta.
 
-# NOTE Not currently used
-def _fetch(post_id=None, api_key=None):
-    assert api_key, "missing API key"
-
-    headers = {'api-key': api_key}
-    if post_id:
-        # Fetch data for given post ID
-        ## NOTE Currently, there's no way to fetch data for a specific post.
-        ## The workaround I'm using here is the best we can do: fetch and search.
-        endpoint = "https://dev.to/api/articles/me/all"
-        post_data = None
-        page = 0
-        while not post_data:
-            page += 1
-            response = requests.get(endpoint, params={ 'page': page }, headers=headers)
-            response.raise_for_status() # raise error if bad request
-            posts = response.json()
-            if posts:
-                post_data = next((data for data in posts if data['id'] == post_id), None)
-            else:
-                break; # No more posts to fetch
-        return post_data
-    else:
-        # Fetch all post data
-        endpoint = "https://dev.to/api/articles/me/all"
-        response = requests.get(endpoint, headers=headers)
-        response.raise_for_status() # raise error if bad request
-        return response.json()
-
 def _draft(post, api_key=None):
     assert api_key, "missing API key"
     assert post, "missing post"
--- tests/test_dev.py
+++ tests/test_dev.py
@@ -4,36 +4,6 @@ from .mocks import MockPost
 import pytest
 import requests
 import requests_mock
-import re
-
-def test_fetch_error_when_api_key_missing():
-    with pytest.raises(AssertionError):
-        dev._fetch()
-
-def test_fetch_request_all_posts(requests_mock):
-    fake_results = []
-    requests_mock.get("https://dev.to/api/articles/me/all", json=fake_results)
-    results = dev._fetch(api_key='fake_api_key')
-    assert results == fake_results
-
-def test_fetch_request_specific_post(requests_mock):
-    fake_post_id = 13
-    requests_mock.get("https://dev.to/api/articles/me/all", json=[{'id':fake_post_id}])
-    results = dev._fetch(fake_post_id, api_key='fake_api_key')
-    assert results['id'] == fake_post_id
-
-
-def test_fetch_request_invalid_post(requests_mock):
-    invalid_post_id = 13
-    def fake_results(req, con):
-        # Ugh, query string parsing. But they don't expose the params at the top-level, so....
-        if int( re.search(r'page=(\d+)', req.query).group(1) ) == 1:
-            return [{"id": invalid_post_id + 1}]
-        else:
-            return []
-    requests_mock.get("https://dev.to/api/articles/me/all", json=fake_results)
-    results = dev._fetch(invalid_post_id, api_key='fake_api_key')
-    assert results is None
 
 def test_draft_error_when_api_key_missing():
     with pytest.raises(AssertionError):
