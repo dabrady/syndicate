8dfca808b605282289f16dba820a3f90ab798aeb
Daniel Brady <daniel.brady@tapjoy.com>
2020-02-04 12:23:40 +0000

    Document top-level `elsewhere` function

--- syndicate/__init__.py
+++ syndicate/__init__.py
@@ -6,12 +6,44 @@ import os
 import sys
 
 def elsewhere(posts, silos):
+    """
+    Syndicates the given posts to the given silos and returns a dictionary of
+    the results keyed by the silo that generated them.
+
+    If a silo has no defined adapter, it is ignored.
+    If a silo has no defined API key, it is ignored.
+
+    Result dictionary is formatted like so:
+
+        {
+            <silo>: {
+              'added': {
+                 <path/to/post>: <silo id>,
+                 ...
+              },
+              'modified': {
+                 <path/to/post>: <silo id>,
+                 ...
+              }
+            },
+            ...
+        }
+
+    Since not all silos may be in sync, the 'added' posts of one silo may be
+    merely 'modified' by another, and vice versa.
+
+    Where possible, silo adapters should only create posts in a 'draft' or
+    unpublished status, to allow time for review and any platform-specific
+    changes to be made by the author.
+    """
+    if not posts:
+        action_log("No posts to syndicate, nothing to do.")
+        return None
     if not silos:
-        action_log('No silos specified, nothing to see here.')
+        action_log('No silos specified, nothing to do.')
         return None
 
-    # De-dupe.
-    silos = list(set(silos))
+    silos = list(set(silos))  # de-dupe the given list of silos
     action_log(f"You want to publish to these places: {silos}")
 
     specs = {silo:_locate(silo) for silo in silos if _locate(silo)}
@@ -39,13 +71,17 @@ _API_KEY = lambda s: f"{s.upper()}_API_KEY"
 
 @functools.lru_cache(maxsize=10)
 def _locate(silo):
+    """Locates the given silo adapter and returns its Python module name if found."""
+    assert silo, 'missing silo'
     return getattr(importlib.util.find_spec(f'syndicate.silos.{silo.lower()}'), 'name', None)
 
 def _syndicate(silo_spec, api_key, posts):
-    if silo_spec and api_key:
-        return importlib.import_module(silo_spec).syndicate(posts, api_key)
-    else:
-        return None
+    """Loads and invokes the entrypoint of the given silo adaptor, returning the results."""
+    assert silo_spec, 'missing silo spec'
+    assert api_key, 'missing API key'
+    return importlib.import_module(silo_spec).syndicate(posts, api_key)
 
 def _get_api_key(silo):
+    """Returns the API key for the given silo, as defined in the environment."""
+    assert silo, 'missing silo'
     return os.getenv(_API_KEY(silo))
